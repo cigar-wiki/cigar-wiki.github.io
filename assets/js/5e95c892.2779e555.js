0d9d2d909c2abf0169c168849936ce72b8e0b461
cigar-wiki <cigar@triplec.cc>
2025-01-21 13:55:35 +0000

    Deploy website - based on dddd0c29aef9113adc020f0c39f29641136235eb

--- assets/js/5e95c892.2779e555.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkcigra_wiki=self.webpackChunkcigra_wiki||[]).push([[9647],{8122:(e,r,s)=>{s.r(r),s.d(r,{default:()=>k});s(6540);var a=s(4164),c=s(1082),i=s(204),u=s(2831),n=s(1410),t=s(4848);function k(e){return(0,t.jsx)(c.e3,{className:(0,a.A)(i.G.wrapper.docsPages),children:(0,t.jsx)(n.A,{children:(0,u.v)(e.route.routes)})})}}}]);
